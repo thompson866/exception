11e479daedae0ba3fe6596bfdcb546d777db1abc
arxip <arxipov1989@gmail.com>
2023-03-25 00:40:37 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,25 @@ public class Main {
 
     public static void main(String[] args) {
         String login = "java_skypro.go";
+        if (!checkUser(login)) {
+            System.out.println("Введен недопустимый логин");
+        }
+        try {
+            if (login.length() > 20) throw new WrongLoginException();
+        } catch (RuntimeException e) {
+            System.out.println("Логин слишком длинный");
+        }
         String password = "D_1hWiKjjP_9";
-        String confirmPassword = "D_1hWiKjjP_9";
+        if (!checkUser(password)) {
+            System.out.println("Введен недопустимый пароль");
+        }
+        try {
+            if (password.length() > 20) throw new WrongPasswordException();
+        } catch (RuntimeException e) {
+            System.out.println("пароль слишком длинный");
+            String confirmPassword = "D_1hWiKjjP_9";
 
+
+        }
     }
-
 }
